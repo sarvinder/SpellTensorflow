99d3e226a7827700be48b48745f92130ec38ca1a
sarvinder <savi895@gmail.com>
2018-10-10 19:34:55 -0800

    the style transfer file added

---
+++ style_transfer.py
@@ -0,0 +1,21 @@
+""" 
+    THIS PROGRAM IS MADE BY SARVINDERJIT SINGH
+    THIS CONTAINS THE IMPLEMENTATION OF THE 
+    ---------------STYLE TRANSFER-------------
+
+    This uses the www.spell.run
+
+    run these commands in the shell/cmd/terminal 
+
+    install spell with pip install spell
+
+    spell login
+
+    This is the file in which we will train the algorithm
+    to do the style transfer but we will try to use the
+    SPPELL  to train the algorithm rather training on 
+    the local system. 
+    
+     """
+
+
